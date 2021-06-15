4cd51b965b7dc7e79e43bba3a6112727d6afe1c4
Al-Rou <roal.24.alro@gmail.com>
2021-06-14 22:56:46 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LinkedList
+{
+    int value;
+    struct LinkedList* next;
+};
+void insertInList(int entry)
+{
+
+}
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
