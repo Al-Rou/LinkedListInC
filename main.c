597a76c495b418c2b6630d9d923ea1d30f8b261a
Al-Rou <roal.24.alro@gmail.com>
2021-06-14 23:27:35 -0400

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,20 @@ struct LinkedList
 struct LinkedList* insertInListOnHead(struct LinkedList* inputList, int entry)
 {
    struct LinkedList* newElement;
+   newElement = (struct LinkedList*) malloc(sizeof(struct LinkedList));
    newElement->value = entry;
    newElement->next = NULL;
+   if(inputList == NULL)
+   {
+       inputList = newElement;
+       return inputList;
+   }
+   else
+   {
+       newElement->next = inputList;
+       inputList = newElement;
+       return inputList;
+   }
 }
 struct LinkedList* insertInListOnTail(struct LinkedList* inputList, int entry)
 {
@@ -18,6 +30,17 @@ struct LinkedList* insertInListOnTail(struct LinkedList* inputList, int entry)
 }
 int main()
 {
+    struct LinkedList* myList;
+    myList = (struct LinkedList*) malloc(sizeof(struct LinkedList));
+    myList = insertInListOnHead(myList, 5);
+    myList = insertInListOnHead(myList, 7);
+    myList = insertInListOnHead(myList, 8);
+    while(myList != NULL)
+    {
+        printf("%d", myList->value);
+        myList = myList->next;
+        //printf("%d", myList->value);
+    }
     printf("Hello world!\n");
     return 0;
 }
